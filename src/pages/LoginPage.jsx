a23266d492e27ee79a5d6f45bd76d4b975431fef
Joe <ngwujoseph90@gmail.com>
2025-10-10 15:19:46 +0100

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -47,7 +47,7 @@ export default function LoginPage() {
 
   return (
     <div
-      className="min-h-screen flex items-center justify-center bg-gray-900 bg-cover bg-center"
+      className="h-screen flex items-center justify-center bg-gray-900 bg-cover bg-center"
       style={{
         backgroundImage:
           "url('/Images/Bank.webp')",
